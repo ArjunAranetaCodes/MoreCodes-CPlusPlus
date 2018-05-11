740343941cd7cacbe85dd9494d6528b396f84268
Arjun Araneta <arjunaraneta@gmail.com>
2018-05-11 08:40:01 +0800

    2018 Updates

---
+++ Conversions/problem3.cpp
@@ -0,0 +1,12 @@
+//Problem 3: Write a program that converts an integer to float.
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int num = 10;
+ 
+    cout << (float)(num);
+ 
+    return 0;
+}
