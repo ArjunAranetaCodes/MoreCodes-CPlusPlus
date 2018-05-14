091d83a372026d7ad0327cd9390044afdf22d3f7
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-05-14 09:27:52 +0800

    2018 Updates

---
+++ Conversions/problem17.cpp
@@ -0,0 +1,12 @@
+//Problem 17: Write a program that converts kilometer to meter.
+#include <iostream>
+
+using namespace std;
+int main(){
+ int kilometer = 1;
+ int meter = kilometer * 1000;
+ 
+ cout << meter;
+
+ return 0;
+}
