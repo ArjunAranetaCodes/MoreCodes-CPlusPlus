bb71f66f74ffaf7ccda7f0da44ecd9533b6daa4e
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-05-14 09:27:39 +0800

    2018 Updates

---
+++ Conversions/problem16.cpp
@@ -0,0 +1,12 @@
+//Problem 16: Write a program that converts feet to inch.
+#include <iostream>
+
+using namespace std;
+int main(){
+ int feet = 1;
+ int inches = feet * 12;
+ 
+ cout << inches;
+
+ return 0;
+}
