ff43ae3046361f5f8f14bfaf15cd53f958a39a7b
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-05-14 09:28:06 +0800

    2018 Updates

---
+++ Conversions/problem18.cpp
@@ -0,0 +1,12 @@
+//Problem 18: Write a program that converts liter to milliliter.
+#include <iostream>
+
+using namespace std;
+int main(){
+ int liter = 1;
+ int milliliter = liter * 1000;
+ 
+ cout << milliliter;
+
+ return 0;
+}
